890dcd643821421d5e45428c5cd16781d7288ce2
Vishal Chaware <30528484+vishalchaware@users.noreply.github.com>
2022-04-08 18:47:19 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const Msg = require('./models/messages');
+const io = require('socket.io')(3000)
+const mongoDB = 'mongodb+srv://vishal:vishal@vishalchaware.b2i6v.mongodb.net/message-database?retryWrites=true&w=majority';
+
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+    console.log('connected')
+}).catch(err => console.log(err))
+io.on('connection', (socket) => {
+    Msg.find().then(result => {
+        socket.emit('output-messages', result)
+    })
+    console.log('a user connected');
+    socket.emit('message', 'Hello world');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+    socket.on('chatmessage', msg => {
+        const message = new Msg({ msg });
+        message.save().then(() => {
+            io.emit('message', msg)
+        })
+
+
+    })
+});
